5b5382b593925f3ca0a2515e9fc0cb61c6e8b211
Oaksas <aman.getnet2@gmail.com>
2021-02-18 22:03:20 -1100

    OOP1.js Commit

--- Lab 08/asset/js/05(OOP1).js
+++ Lab 08/asset/js/05(OOP1).js
@@ -2,9 +2,14 @@
 function Person(firstName, lastName) {
 
     // 1. set the firstName and lastName using this keyword
+    this.firstName = firstName
+    this.lastName = lastName
 
 
     //2. Create a greeting function returns [use this keyword to get the first and last name]
+    this.greeting =function(){
+          return `Hi ${this.firstName} ${this.lastName}`
+    }
     //  ==> Hi , firstName lastName
 
 
@@ -12,7 +17,8 @@ function Person(firstName, lastName) {
 }
 
 //3. Create Jon Whose full name is ["Jon", "Snow"] Object using new keyword [new ConstructorFun()]
+const Jon = new Person("Jon", "Snow")
 
 
 //4. Display the greeting on Jon Object [remove the string when you have the object]
-Obj_C1.innerHTML = "Jon.greeting()";
+Obj_C1.innerHTML = Jon.greeting();
