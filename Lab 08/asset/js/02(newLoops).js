79fab0c1e4aa455f96dbc6a532fc880f304a057d
Oaksas <aman.getnet2@gmail.com>
2021-02-18 20:42:38 -1100

    newLoops.js Commit

--- Lab 08/asset/js/02(newLoops).js
+++ Lab 08/asset/js/02(newLoops).js
@@ -13,6 +13,13 @@ function forINLoop() {
     const student = { name: "Abebe", age: 21, year: "2dn year" }
 
     // 1. Create a for in loop that iterate over the student object
+    for (const s in student){
+        forInLoop.innerHTML +=
+        `
+        <li class="list-group-item" >${s} : ${student[s]}</li>
+
+    `;
+    }
 
 
     // 2. Inside the loop paint the UI [Use the Format Given Below]
@@ -20,11 +27,7 @@ function forINLoop() {
 
     //Use this Format 
 
-    // forInLoop.innerHTML +=
-    //     `
-    //     <li class="list-group-item" >${Key} : ${Value}</li>
-
-    // `;
+    
 
 }
 
@@ -36,16 +39,18 @@ function forOFLoop() {
 
     // 1. Create a for of loop that iterate over the students Array and displays the names of students 
 
+for(const s of students){
+    forOfLoop.innerHTML +=
+    `
+    <li class="list-group-item" >Name : Abebe</li>
 
+`;
+}
     // 2. Inside the loop paint the UI [Use the Format Given Below]
 
 
     //Use this Format 
 
-    // forOfLoop.innerHTML +=
-    //     `
-    //     <li class="list-group-item" >Name : Abebe</li>
-
-    // `;
+    
 
 }
